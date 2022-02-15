fdfc9cf8dd790a8352c3efbd904e2f721ad7a688
LPAube <lpaube@hotmail.com>
2022-02-15 17:21:30 -0500

    Add: List of functions in Vector

--- vector.hpp
+++ vector.hpp
@@ -6,46 +6,51 @@
 /*   By: laube <louis-philippe.aube@hotmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 17:11:36 by laube             #+#    #+#             */
-/*   Updated: 2022/02/13 22:26:04 by laube            ###   ########.fr       */
+/*   Updated: 2022/02/15 17:20:22 by laube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template<typename T>
-class VectorIterator
-{
-	public:
-		VectorIterator(): p(nullptr) {} // Default constructor
-		VectorIterator(pointer p): p(p) {}
-		VectorIterator(const VectorIterator& other): p(other.p) {} // Copy constructor
-		virtual ~VectorIterator() {} // Destructor
+#include <memory>
 
-		VectorIterator &operator=(const VectorIterator& other) {
-			this->p = other.p;
-			return (*this);
-		}
+template<class T, class Allocator = std::allocator<T>>
 
-		T&	operator*() {
-			return (*this->p);
-		}
-		const T& operator*() const {
-			return (*this->p);
-		}
-};
 
-template<typename T>
-class Vector
-{
-	private:
-		T* m_container;
-		unsigned long	m_capacity;
-		unsigned long	m_size;
-
-	public:
-		Vector(): m_container(nullptr), m_capacity(0), m_size(0) {}
-		Vector(unsigned long n, const T & val=T()): m_container(nullptr), m_capacity(0), m_size(0) {
-			this->assign(n, val);
-		}
-		void assign(iterator first, iterator last) {
-			size_t
-		}
-};
+// https://en.cppreference.com/w/cpp/container/vector
+/* LIST 
+ *
+ * MEMBER FUNCTIONS
+ * 		CONSTRUCTORS
+ * 			1.	vector();
+ * 			2.	explicit vector( const Allocator& alloc );
+ * 			3.	explicit vector( size_type count, const T& value = T(), const Allocator& alloc = Allocator());
+ * 			5.	template< class InputIt >vector( InputIt first, InputIt last, const Allocator& alloc = Allocator() );
+ * 			6.	vector( const vector& other );
+ *
+ * 		DESTRUCTORS
+ * 			1.	~vector();
+ *
+ * 		OTHER MEMBER FUNCTIONS
+ * 			1.	vector& operator=( const vector& other );
+ * 			1.	void assign( size_type count, const T& value );
+ * 			2.	template< class InputIt >void assign( InputIt first, InputIt last );
+ * 			1.	allocator_type get_allocator() const;
+ *
+ * 		ELEMENT ACCESS
+ * 			1.	reference at( size_type pos );
+ * 			2.	const_reference at( size_type pos ) const;
+ * 			1.	reference front();
+ * 			2.	const_reference front() const;
+ * 			1.	reference back();
+ * 			2.	const_reference back() const;
+ * 			1.	T* data();
+ * 			2.	const T* data() const;
+ *
+ * 		ITERATORS
+ * 			1.	iterator begin();
+ * 			2.	const_iterator begin() const;
+ * 			1.	iterator end();
+ * 			2.	const_iterator end() const;
+ * 			1.	reverse_iterator rbegin();
+ * 			2.	const_reverse_iterator rbegin() const;
+ * 			1.	reverse_iterator rend();
+ * 			2.	const_reverse_iterator rend() const;
