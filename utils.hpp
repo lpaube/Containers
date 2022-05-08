511ef97afb376d6c09dc411be92c558a8cf808c3
LPAube <lpaube@hotmail.com>
2022-05-08 14:34:18 -0400

    WIP: Working on is_integral type_traits

--- utils.hpp
+++ utils.hpp
@@ -7,3 +7,47 @@ template <typename T>
 struct enable_if<true, T> {
   typedef T type;
 };
+
+// Implementation of type_traits
+struct true_type {};
+struct false_type {};
+
+// Implementation for are_same
+template <typename, typename>
+struct are_same
+{
+  bool value = false;
+  typedef false_type type;
+};
+
+template <typename T>
+struct are_same<T, T>
+{
+  bool value = true;
+  typedef true_type type;
+};
+
+// Implementation of is_integral
+template <typename T>
+struct is_integral
+{
+  typedef false_type type;
+  typedef bool value_type;
+
+  bool value = false;
+  operator bool() {
+    return value;
+  }
+};
+
+template <>
+struct is_integral<bool>
+{
+  typedef true_type type;
+  typedef bool value_type;
+
+  bool value = true;
+  operator bool() {
+    return value;
+  }
+};
