cf63edea2a0c0fddfef09d92854ee2e55d0af4be
LPAube <lpaube@hotmail.com>
2022-05-11 14:10:55 -0400

    working on map and rb_tree

--- map.hpp
+++ map.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include "rb_tree.hpp"
 
 namespace ft {
   template <typename Key, typename T, typename Compare = std::less<Key>, typename Allocator = std::allocator<std::pair<const Key, T> > >
@@ -12,7 +13,7 @@ namespace ft {
        */
       typedef  Key                                            key_type;                                                      
       typedef  T                                              mapped_type;                                                   
-      typedef  std::pair<const Key,T>                         value_type;
+      typedef  std::pair<const Key, T>                        value_type;
       typedef  std::size_t                                    size_type;                                                     
       typedef  std::ptrdiff_t                                 difference_type;                                               
       typedef  Compare                                        key_compare;                                                   
@@ -26,8 +27,9 @@ namespace ft {
       typedef  typename ft::reverse_iterator<iterator>      reverse_iterator;        
       typedef  typename ft::reverse_iterator<const_iterator>  const_reverse_iterator;  
 
-      protected:
+      private:
         rb_tree tree;
+        Allocator alloc;
 
       /*
        * std::map::value_compare is a function object that compares objects 
--- rb_tree.hpp
+++ rb_tree.hpp
@@ -1,3 +1,21 @@
 namespace ft {
-  
+  template <typename Tp>
+  class rb_tree
+  {
+    public:
+      typedef Tp value_type;
+      //typedef rb_tree_iterator<value_type> iterator;
+
+    struct tree_node 
+    {
+      bool  is_black;
+      tree_node* parent;
+      tree_node* left;
+      tree_node* right;
+      Tp value;
+    };
+
+    private:
+      tree_node*  begin_node;
+  };
 }
